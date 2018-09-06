61dc5851ce8351ca7d87bf9b2f432b510690dd83
sigsant <36236434+sigsant@users.noreply.github.com>
2018-09-06 21:22:34 +0200

    validacion de errores
    Aparece un mensaje de error si uno de los campos está vacio o no coinciden.

--- js/login.js
+++ js/login.js
@@ -2,11 +2,13 @@ document.querySelector(".button").onclick = accesoHome;
 document.querySelector('#bot-rad').onclick = mostrarPassword;
 
 // Valida el acceso a la página web sólo si coincide los valores de email y contraseña
+// Si es valido: redigirir a la pagina principal; si no: anunciar error
 function accesoHome() {
     var email = document.querySelector("#email").value;
     var password = document.querySelector("#password").value;
+    var errorValidacion = document.querySelector("#error");
 
-    return email == password ? console.log("Correcto") : console.log("Incorrecto");
+    return email == password && email != "" ? window.location.href="home.html" : errorValidacion.classList.toggle("ocultar");
 }
 
 // Oculta o muestra la contraseña como texto plano
